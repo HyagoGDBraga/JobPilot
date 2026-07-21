01193bf783a4303f34962d79ef7e55b0d7e0d6ee
Hyaguiin <137925587+Hyaguiin@users.noreply.github.com>
2026-07-21 00:32:37 -0300

    feat(middleware): error-global-middleware

--- src/modules/http/index.ts
+++ src/modules/http/index.ts
@@ -3,13 +3,14 @@ import cors from "cors";
 import { allowedDomains } from "../../env/env.zod";
 import healthCheckRouter from "../app/router/app.route";
 import database_Source from "../infra/database/router/db.router";
-import hugging_FACE from '../llm/router/ai.router';
+import hugging_FACE from "../llm/router/ai.router";
+import { errorMiddleware } from "../../middlewares/error-middleware";
 const application = express();
 
 const corsOptions = {
   origin: (
     origin: string | undefined,
-    callback: (error: Error | null, success?: boolean) => void
+    callback: (error: Error | null, success?: boolean) => void,
   ) => {
     if (!origin || !allowedDomains.includes(origin)) {
       callback(null, true);
@@ -24,6 +25,6 @@ application.use(cors(corsOptions));
 application.use("/", healthCheckRouter);
 application.use("/", database_Source);
 application.use("/ai", hugging_FACE);
-
+application.use(errorMiddleware);
 
 export default application;
